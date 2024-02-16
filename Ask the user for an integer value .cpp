12ae16717f41988d46b8376052ed33ee751ceda4
MD.SIRAJUL ISLAM <155076937+hmsirajul@users.noreply.github.com>
2024-02-16 16:17:27 +0600

    Add files via upload

---
+++ Ask the user for an integer value .cpp
@@ -0,0 +1,24 @@
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//    int i ;
+//    cout << "Enter the value :";
+//    cin >> i;
+//    cout << "Here is your number:" | << i << \n;
+//}
+//#include<iostream>
+//using namespace std;
+//
+//int main ()
+//{
+//    int i;
+//    float f;
+//    char s[80];
+//
+//    cout << "Enter an intger ,float, and string ";
+//    cin >> i >> f >> s;
+//    cout << "Here is your data: ";
+//    cout << i <<  f << s;
+//    return 0;
+//}
